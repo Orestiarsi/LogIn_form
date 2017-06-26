6df0c498d382cbf10c26e83d8d58c3724f638919
OrestiArsi <orestiarsi@gmail.com>
2017-06-26 15:53:41 -0400

    first commit

---
+++ userData.js
@@ -0,0 +1,28 @@
+module.exports = {
+  users: [
+    {
+      username: "oresti",
+      password: "go on"
+    },
+    {
+      username: "brittany",
+      password: "denied"
+    },
+    {
+      username: "dea",
+      password: "boss"
+    },
+    {
+      username: "ardi",
+      password: "the_man"
+    },
+    {
+      username: "ajla",
+      password: "crazy"
+    },
+    {
+      username: "era",
+      password: "baby"
+    },
+  ]
+}
